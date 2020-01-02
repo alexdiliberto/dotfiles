86013b663082e24ce9dd3731a8700c9c86f964c6
Alex DiLiberto <alex@alexdiliberto.com>
2020-01-01 23:26:42 -0500

    Revert "remove pinentry-mac brew formula"
    This reverts commit 3424d26df25f3f30cd6176e90f3b72efbb5eebe8.

--- brew.sh
+++ brew.sh
@@ -112,6 +112,7 @@ brew install gpg-agent
 brew install heroku
 brew install jq
 brew install mongodb
+brew install pinentry-mac
 brew install postgresql
 brew install puma/puma/puma-dev
 brew install rbenv
